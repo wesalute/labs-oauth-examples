468bcf50cb56d4f601f89c40c3f0bece701f2114
Leonid Makarov <leonid.makarov@ffwagency.com>
2021-11-29 15:27:25 -0800

    Updated next.config.js to support BASE_PATH configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
+/*
+ * Gets the BASE_PATH from the command used to start this app.
+ * If BASE_PATH is specified but it does not start with a "/" 
+ * then add it. 
+ */
+function getBasePath() {
+  var basePath = ''
+
+  if (process.env.BASE_PATH) {
+      if (process.env.BASE_PATH.startsWith("/") ) {
+          basePath = process.env.BASE_PATH;
+      } else {
+          basePath = "/" + process.env.BASE_PATH;
+      }
+  } 
+
+  return basePath
+}
+
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  basePath: getBasePath(),
+  assetPrefix: getBasePath() ,
+  publicRuntimeConfig: {
+    basePath: getBasePath() ,
+  }
 }
