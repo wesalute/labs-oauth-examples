e8944f473cfc5b4fd450232274d3e099fc8fb34b
Matt Korostoff <MKorostoff@gmail.com>
2021-12-07 11:45:56 -0500

    Add upsell example

--- pages/api/oauth/redirect.js
+++ pages/api/oauth/redirect.js
@@ -1,6 +1,6 @@
 import { getRedirect } from 'lib/oauth';
 
 export default function handler(req, res) {
-  const redirect = getRedirect(req.headers.host, req.query.client_id);
+  const redirect = getRedirect(req.headers.host, req.query.client_id, req.query.premium);
   res.redirect(redirect);
 }
