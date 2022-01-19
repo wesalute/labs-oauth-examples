a2010f256a67d27c5b6602fae3e75569a2dec16f
Matt Korostoff <MKorostoff@gmail.com>
2022-01-19 16:47:26 -0500

    Update brand connections homepage text

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <p className={styles.intro}>The following pages show a variety of simulated experiences in which brands connect to WeSalute using OAuth. Using these pages, simulated consumers may connect their WeSalute account to simulated third parties.</p>
+        <p className={styles.intro}>The following pages show a variety of simulated experiences in which brands connect to WeSalute using OAuth. Using these pages, simulated consumers may connect their WeSalute account to simulated third parties.
+        You may view and edit your connected brands on the <a href="https://checkout-dev.veteransadvantage.com/account/brand-connections">brand connections manager.</a></p>
 
         <h2>Please Select Your Simulation:</h2>
         <ul>
-          <li><Link href="/amazon">Amazon</Link></li>
-          <li><Link href="/starbucks">Starbucks</Link></li>
-          <li><Link href="/cart">Generic Shopping Cart</Link></li>
-          <li><Link href="/robinhood">User Registration</Link></li>
-          <li><Link href="/ourside">Ourside</Link></li>
-          <li><Link href="/upsell">Upsell</Link></li>
-          <li><Link href="/verified-offer">Verification Required</Link></li>
+          <li className={styles.listItem}><Link href="/amazon">Amazon</Link> - Connect your WeSalute account to a simulated amazon.com.</li>
+          <li className={styles.listItem}><Link href="/starbucks">Starbucks</Link>- Connect your WeSalute account to a simulated starbucks.com.</li>
+          <li className={styles.listItem}><Link href="/cart">Generic Shopping Cart</Link>- Connect your WeSalute account during checkout at an online store without disrupting the purchase flow.</li>
+          <li className={styles.listItem}><Link href="/robinhood">User Registration</Link>- "Login with WeSalute" to newly register on partner sites. Similar to "login with facebook" or "login with google."</li>
+          <li className={styles.listItem}><Link href="/ourside">Ourside</Link>- Connect your WeSalute account to a partner website, starting from a link hosted outside of the partner website. Useful for hosting offer links on veteransadvantage.com, in email messages, or on marketing sites.</li>
+          <li className={styles.listItem}><Link href="/upsell">Premium Required</Link>- Connect your WeSalute account to a simulated amazon.com, but first require upgrading to a paid Vet Rewards account.</li>
+          <li className={styles.listItem}><Link href="/verified-offer">Verification Required</Link>- Connect your WeSalute account to a simulated amazon.com, but first require verification through a government database lookup or secure document upload.</li>
         </ul>
       </main>
       
