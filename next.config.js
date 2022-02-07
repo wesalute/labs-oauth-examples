973d2c02dbeebbbf10ca106a6d9f5b5afa7eacdb
Matt Korostoff <MKorostoff@gmail.com>
2022-02-07 10:56:43 -0500

    Add production configuration

--- next.config.js
+++ next.config.js
@@ -19,6 +19,26 @@ function getBasePath() {
 function getProtocol() {
   return (process.env.PROTOCOL && process.env.PROTOCOL == 'https') ? 'https' : 'http';
 }
+function getClients() {
+  const clients = {
+    "amazon": {id: 'amazon-dev', secret: 'ERJV0XsNclYs._2lSNunue1_70'},
+    "upsell": {id: 'upsell', secret: '8-RBdv~-A19ENyituvK8MHI8Ax'},
+    "verified-offer": {id: 'verified-offer', secret: 'tBB1sD00i0S7VdZ03af1H8_d7l'},
+    "starbucks": {id: 'starbucks-dev', secret: 'd1Xbue_2sh~Q.s.ezZVMSrzcLz'},
+    "cart": {id: 'bluline-dev', secret: '3FOD7Umt~dLCIQNIH4mXByvzV2'},
+    "robinhood": {id: 'robinhood-dev', secret: 'GwiTYSBfWCz1VqZkjk0zHg_RoP'},
+    "ourside": {id: 'paramount-dev', secret: 's-3CerDpR~YF4Q6erVnvKjk8ns'},
+  }
+  return (process.env.CLOUD_ENV === 'dev') ? clients : null;
+}
+function getUserInfoUrl() {
+  const url = "https://checkout-dev.veteransadvantage.com/api/partner/member";
+  return (process.env.CLOUD_ENV === 'dev') ? url : null;
+}
+function getOauthUrl() {
+  const url = "https://k8s-dev.veteransadvantage.com";
+  return (process.env.CLOUD_ENV === 'dev') ? url : null;
+}
 
 module.exports = {
   reactStrictMode: true,
@@ -26,7 +46,14 @@ module.exports = {
   assetPrefix: getBasePath(),
   publicRuntimeConfig: {
     basePath: getBasePath(),
-    protocol: getProtocol()
+    protocol: getProtocol(),
+  },
+  //When this app runs in the WeSalute dev environment, we will connect
+  //to the dev instance of all external services
+  serverRuntimeConfig: {
+    clients: getClients(),
+    userInfoUrl: getUserInfoUrl(),
+    oauthUrl: getOauthUrl()
   },
   // Path rewrites config
   async rewrites() {
--- pages/api/oauth/userinfo.js
+++ pages/api/oauth/userinfo.js
@@ -1,6 +1,7 @@
 //Todo: get userinfo endpoint from environment variable
 import { refreshToken } from "lib/oauth";
 import { getCookies, setCookies } from "cookies-next";
+import { serverRuntimeConfig } from 'next.config';
 
 export default async function handler(req, res) {
   //Todo: refresh if expired
@@ -8,6 +9,10 @@ export default async function handler(req, res) {
   const client_id = req.query.client_id;
   const access_token = cookies[`${client_id}_access_token`];
   const refresh_token = cookies[`${client_id}_refresh_token`];
+  
+  let user_info_url = 'https://checkout.veteransadvantage.com/api/partner/member';
+  //Optionally allow this url to be overridden for the dev environment
+  user_info_url = (serverRuntimeConfig.userInfoUrl) ? serverRuntimeConfig.userInfoUrl : user_info_url;
 
   let user = {};
 
