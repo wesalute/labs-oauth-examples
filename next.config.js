55f1b5744ec81685c48033e748aa3d57177be5b4
Leonid Makarov <leonid.makarov@ffwagency.com>
2023-03-01 14:21:39 +0100

    Updated OAuth client credentials for local/dev

--- next.config.js
+++ next.config.js
@@ -22,13 +22,13 @@ function getProtocol() {
 // Override OAuth2 endpoint and client config in the dev environment
 function getClients() {
   const clients = {
-    "amazon": {id: 'amazon-dev', secret: 'ERJV0XsNclYs._2lSNunue1_70'},
-    "upsell": {id: 'upsell', secret: '8-RBdv~-A19ENyituvK8MHI8Ax'},
-    "verified-offer": {id: 'verified-offer', secret: 'tBB1sD00i0S7VdZ03af1H8_d7l'},
-    "starbucks": {id: 'starbucks-dev', secret: 'd1Xbue_2sh~Q.s.ezZVMSrzcLz'},
-    "cart": {id: 'bluline-dev', secret: '3FOD7Umt~dLCIQNIH4mXByvzV2'},
-    "robinhood": {id: 'robinhood-dev', secret: 'GwiTYSBfWCz1VqZkjk0zHg_RoP'},
-    "ourside": {id: 'paramount-dev', secret: 's-3CerDpR~YF4Q6erVnvKjk8ns'},
+    "amazon": {id: 'd73055df-24a9-4313-9968-0cdc99934d9e', secret: 'wAYeR6rLrpR-e7Tq_ikwH8.pkW'},
+    "upsell": {id: 'c103bd6b-c1bf-4973-9383-7a23b2bb460a', secret: 'LTodDiMpa4OnAMUCN-Rb1vyUWf'},
+    "verified-offer": {id: '3666c2f0-9ed6-496d-b108-7d012c17cc3f', secret: 'JT7OhDc1XfILWuzt0-GCMmTvAq'},
+    "starbucks": {id: '9f530122-b931-4e90-9776-f939d2af1218', secret: 'hEOv80fb1qvLTIOivKWDENeak.'},
+    "cart": {id: 'a800a998-fac2-487a-a81d-4855b7906ee9', secret: 'wQNWTzTBgKe4H1v0z3d8PU8GQc'},
+    "robinhood": {id: '99976afe-84d9-45d1-81c0-786acacca1cc', secret: '5Tf3zsXD2oeELQAojYKbOqjsE6'},
+    "ourside": {id: 'd739e52a-2c81-4585-bf43-426694cc566b', secret: 'u4DRjS6Ksx.uo0yPO9U2hG8Iu.'},
   }
   return (process.env.CLOUD_ENV === 'dev') ? clients : null;
 }
@@ -37,7 +37,7 @@ function getUserInfoUrl() {
   return (process.env.CLOUD_ENV === 'dev') ? url : null;
 }
 function getOauthUrl() {
-  const url = "https://idp-dev.wesaluteapis.com";
+  const url = "https://fervent-dewdney-gqdkbt8xn7.projects.oryapis.com";
   return (process.env.CLOUD_ENV === 'dev') ? url : null;
 }
 
